0ba171aad7a7136a667b8613554fe15c1dfc8585
Shreeyash <132891223+ShreeyashPandey@users.noreply.github.com>
2024-04-16 12:45:54 +0530

    cluster name changes

--- create-eks-stack.sh
+++ create-eks-stack.sh
@@ -1,5 +1,5 @@
 aws cloudformation create-stack \
   --region us-east-1 \
-  --stack-name my-eks-cluster \
+  --stack-name sandbox-cluster \
   --capabilities CAPABILITY_NAMED_IAM \
   --template-body file://eks-stack.yaml 
